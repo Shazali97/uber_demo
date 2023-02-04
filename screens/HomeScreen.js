40bb1e79bfa7d0066570f8648601a92184f7b051
Shaz Ali <shazali@Shazs-MBP.attlocal.net>
2023-02-03 22:16:46 -0600

    adding screen functionality and google API

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from 'twrnc';
 import React from 'react'
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -17,6 +23,38 @@ const HomeScreen = () => {
             uri:  "https://links.papareact.com/gzs",
           }}
         />
+      <GooglePlacesAutocomplete 
+        styles={{
+          container: {
+            flex: 0,
+          },
+          textInput: {
+            fontSize: 18,
+          },
+        }}
+
+        onPress={(data, details = null) => {
+          dispatch(setOrigin({
+            location: details.geometry.location,
+            description: data.description
+          }))
+
+          dispatch(setDestination(null))
+        }}
+        fetchDetails={true}
+        enablePoweredByContainer={false}
+        returnKeyType={"search"}
+        minLength={2}
+        query={{
+          key: GOOGLE_MAPS_APIKEY,
+          language: 'en'
+        }}
+      
+
+        placeholder="Where from?"
+        nearbyPlacesAPI="GooglePlacesSearch"
+        debounce={400}
+      />
       <NavOptions/>
       </View>
     </SafeAreaView>
